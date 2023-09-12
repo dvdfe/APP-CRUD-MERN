c78a99815252c2f4e0f790d2a74dbec592618d41
DVDFE <davidfe.pro@gmail.com>
2023-09-12 15:44:53 +0200

    configuration de redux toolkit dans l'app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
-import Routes from "./components/Routes/index"
+import Routes from "./components/Routes/index";
+import { Provider, useDispatch } from "react-redux";
+import store from "./app/store";
+import { useEffect } from "react";
+import axios from "axios";
+import { setUsersData } from "./feature/usersSlice";
+import { useState } from "react";
 
 function App() {
+  const [uid, setUid] = useState(null);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/user/login")
+      .then((res) => dispatch(setUsersData(res.data)));
+  }, [uid]);
+
   return (
-    <div>
-      <Routes/>
-    </div>
+    <Provider store={store}>
+      <Routes />
+    </Provider>
   );
 }
 
