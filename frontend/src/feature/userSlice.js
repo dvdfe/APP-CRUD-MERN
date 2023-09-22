82a6e73a66b684da41086d978541a53797940122
DVDFE <davidfe.pro@gmail.com>
2023-09-22 13:27:18 +0200

    ajout logique followers et following + popup + scss

--- frontend/src/feature/userSlice.js
+++ frontend/src/feature/userSlice.js
@@ -2,17 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const userDataSlice = createSlice({
   name: "user",
-  initialState: null, // L'état initial est null, car l'utilisateur n'est pas encore connecté.
+  initialState: { data: null, allUsers: [] }, // Vous pouvez utiliser un objet pour stocker à la fois les données de l'utilisateur et la liste de tous les utilisateurs.
   reducers: {
     setUserData: (state, action) => {
-      return action.payload; // Met à jour l'ID de l'utilisateur avec la valeur fournie dans l'action.
+      state.data = action.payload; // Met à jour les données de l'utilisateur avec la valeur fournie dans l'action.
     },
-    getAllUsers:(state, action) => {
-      return action.payload;
-
+    getAllUsers: (state, action) => {
+      state.allUsers = action.payload; // Met à jour la liste de tous les utilisateurs avec la valeur fournie dans l'action.
     },
     clearUserData: (state) => {
-      return null; // Réinitialise l'ID de l'utilisateur à null lorsque l'utilisateur se déconnecte.
+      state.data = null; // Réinitialise les données de l'utilisateur à null lorsque l'utilisateur se déconnecte.
     },
   },
 });
--- frontend/src/pages/Profil.js
+++ frontend/src/pages/Profil.js
@@ -1,15 +1,15 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import Navigation from "../components/Navigation";
 import { NavLink } from "react-router-dom";
 
 const Profil = () => {
-  const userData = useSelector((state) => state.user);
+  const userData = useSelector((state) => state.user.data);
+  const allUsers = useSelector((state) => state.user.allUsers); // Assurez-vous de récupérer le tableau d'utilisateurs
+
   const [followingPopup, setFollowingPopup] = useState(false);
   const [followersPopup, setFollowersPopup] = useState(false);
-
-  
-
+  console.log("allU:", allUsers);
   return (
     <div className="page-container">
       <Navigation />
@@ -29,26 +29,67 @@ const Profil = () => {
               onClick={() => setFollowersPopup(true)}
               className="follower-count"
             >
-              {userData ? userData.followers.length : ""} follower(s)
+              {userData && userData.followers ? userData.followers.length : ""}
+              follower(s)
             </div>
             <div
               onClick={() => setFollowingPopup(true)}
               className="following-count"
             >
-              {userData ? userData.following.length : ""} suivi(e)s
+              {userData && userData.following ? userData.following.length : ""}
+              suivi(e)s
             </div>
           </div>
           <div className="name-container">{userData.name}</div>
           <div className="bio-container">{userData.bio}</div>
         </div>
       </div>
+
       {followersPopup && (
         <div className="popup-profil-container">
           <div className="modal">
+            <span className="cross" onClick={() => setFollowersPopup(false)}>
+              &#10005;
+            </span>
             <h3>Followers</h3>
-            <span className="cross" onClick={()=>setFollowersPopup(false)}>&#10005;</span>
-            <ul>
+            <ul className="followers-list">
+              {allUsers &&
+                allUsers
+                  .filter((user) => userData.followers.includes(user._id))
+                  .map((user) => (
+                    <li key={user._id}>
+                      <img
+                        src={user.picture}
+                        alt="photo de profil utilisateur"
+                      />
+                      <h4>{user.pseudo}</h4>
+                    </li>
+                  ))}
+            </ul>
+          </div>
+        </div>
+      )}
 
+      {followingPopup && (
+        <div className="popup-profil-container">
+          <div className="modal">
+            <span className="cross" onClick={() => setFollowingPopup(false)}>
+              &#10005;
+            </span>
+            <h3>Suivi(e)s</h3>
+            <ul className="following-list">
+              {allUsers &&
+                allUsers
+                  .filter((user) => userData.following.includes(user._id))
+                  .map((user) => (
+                    <li key={user._id}>
+                      <img
+                        src={user.picture}
+                        alt="photo de profil utilisateur"
+                      />
+                      <h4>{user.pseudo}</h4>
+                    </li>
+                  ))}
             </ul>
           </div>
         </div>
